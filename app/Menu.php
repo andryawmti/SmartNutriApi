7d9c4964da052ce9afe44d20ab5d5c33e987b9d6
Heri Andria Herdiana <34875303+andryawmti@users.noreply.github.com>
2019-01-19 21:33:33 +0700

    Revert "Feature/menu list api (#10)"
    This reverts commit a8d824ab78a3ba59e947d5c6cbc03112c69377b4.

--- app/Menu.php
+++ app/Menu.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class Menu extends Model
 {
@@ -15,45 +14,5 @@ class Menu extends Model
     public function menuItems()
     {
         return $this->hasMany('App\MenuItem');
-    }
-
-    public static function getMenuSuggestionByCalorieNeed($calorie)
-    {
-        $menuSuggestion = [];
-
-        $caloriePerMenu = (double)$calorie / 3;
-        $caloriePerMenu = ceil($caloriePerMenu);
-
-        $sql = "SELECT *, ABS($caloriePerMenu - calorie) AS delta_calorie FROM menu_helper WHERE ABS($caloriePerMenu - calorie) <= 20";
-        $orderBy = " ORDER BY delta_calorie ASC";
-
-        $firstMenu = DB::select($sql . $orderBy);
-        $lengthOne = count($firstMenu);
-
-        if ($lengthOne) {
-            $firstMenu = $firstMenu[(int)rand(0, $lengthOne - 1)];
-            $menuSuggestion[] = $firstMenu;
-        }
-
-        $secondMenu = false;
-        if ($firstMenu) {
-            $secondMenu = DB::select($sql . 'WHERE id != ' . $firstMenu['id'] . $orderBy);
-            $lengthTwo = count($secondMenu);
-            if ($lengthTwo) {
-                $secondMenu = $secondMenu[(int)(rand(0, $lengthTwo - 1))];
-                $menuSuggestion[] = $secondMenu;
-            }
-        }
-
-        if ($secondMenu) {
-            $thirdMenu = DB::select($sql . 'WHERE id != ' . $secondMenu['id']);
-            $lengthThree = count($thirdMenu);
-            if ($lengthThree) {
-                $thirdMenu = $thirdMenu[(int)(rand(0, $lengthThree - 1))];
-                $menuSuggestion[] = $thirdMenu;
-            }
-        }
-
-        return $menuSuggestion;
     }
 }
