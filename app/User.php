09040c5eb7da604c51f5d414e24fac966fdce3b9
Heri Andria Herdiana <34875303+andryawmti@users.noreply.github.com>
2019-01-19 15:06:11 +0700

    Revert "Feature/menu list api (#6)"
    This reverts commit b8c8619d1ca2f521db713c7ab4ce576b9499ff8c.

--- app/User.php
+++ app/User.php
@@ -3,24 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Hash;
 
 class User extends Model{
     public function consultation()
     {
         $this->hasMany('App\Consultation');
-    }
-
-    public static function findUserByEmailAndPassword($email, $password)
-    {
-        $user = User::where([
-            ['email', '=', $email],
-        ])->first();
-
-        if ($user && Hash::check($password, $user->password)) {
-            return $user;
-        }
-
-        return false;
     }
 }
